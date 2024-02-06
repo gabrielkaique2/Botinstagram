422c70823994ecf89237ca67988105b29ee7d6ac
Gabriel ROdrigues <gabrielkaique_contato@hotmail.com>
2024-02-05 23:15:18 -0300

    sdmas

---
+++ teste.py
@@ -0,0 +1,5 @@
+def relation_to_luke(name):
+	family = {'Darth Vader':'father','Leia':'sister','Han':'brother in law','R2D2':'droid'}
+	return print(f'Luke, I am your {family[name]}')
+	
+relation_to_luke('Han')
