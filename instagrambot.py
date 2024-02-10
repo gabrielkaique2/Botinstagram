efb12a4d9dbc1a53414f342d130107dc9a18acc2
Gabriel ROdrigues <gabrielkaique_contato@hotmail.com>
2024-02-10 01:00:22 -0300

    versão funcional 1.0

--- instagrambot.py
+++ instagrambot.py
@@ -1,4 +1,3 @@
-#from wdpage import WDPage
 from selenium import webdriver;
 from selenium.webdriver.common.keys import Keys;
 from selenium.webdriver.common.by import By;
@@ -6,7 +5,6 @@ from selenium.webdriver.support.wait import WebDriverWait;
 from selenium.webdriver.support import expected_conditions as EC;
 import random
 from time import sleep;
-
 
 class InstagramBot():
     def __init__(self,username,password):
@@ -27,34 +25,26 @@ class InstagramBot():
         password_field.send_keys(self.password)
         sleep(3)
         password_field.send_keys(Keys.RETURN)
-        sleep(3)
-        print('login bem sucedido')
+        sleep(10)
+        print(f'{self}: Login bem sucedido')
 
     def GoToLink(self,link):
         driver = self.driver
-
         driver.get(link)
-        #iframe = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'iframe')))
-        #driver.switch_to.frame(iframe)
         sleep(5)
-        print('link acessado')
+        print(f'{self}: Link acessado')
 
-    #antigo nome Coment() -> novo nome getField()
     def getField(self):
         driver = self.driver
-
         #text_field é a variavel que armazena o XPATH do campo de texto dos comentário.
-        
         text_field = driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[4]/section/div/form/div/textarea')
         sleep(1)
         text_field.click()
         sleep(1)
-
         #após o clique, deve redefinir a variavel text_field com o mesmo XPATH do campo! por algum motivo o webdriver perde a referência da DOM!
-
         text_field = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[4]/section/div/form/div/textarea')
         #sleep(1)
-        print('campos de comentários encontrados')
+        print(f'{self}: Campo encontrado')
         return text_field
 
     def SendComment(self):
@@ -62,8 +52,10 @@ class InstagramBot():
         
         #função que encontra o botão de enviar comentário e clica nele para enviar! é necessário separar ele da função getField para não haver problemas de continuidade!
 
-        subitComment = WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,"//*[@id='mount_0_0_wl']/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[4]/section/div/form/div/div[2]/div")))
-        subitComment.click()
+        submitComment = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[4]/section/div/form/div/textarea')
+        submitComment.send_keys(Keys.RETURN)
+        print(f"{self}: comentário enviado com sucesso")
+        #submitComment.clear()
 
     def ToChoose(self, num_to_choose):
         #em nomes será adicionado os IG's
@@ -79,31 +71,11 @@ class InstagramBot():
             escolhido = random.choice(nomes)   
             if escolhido not in chosen:
                 chosen.append(escolhido)
-
         # Concatena os elementos da lista chosen em uma única string separada por espaços
         chosen_string = ' '.join(chosen)
-    
+        print(f'{self}: IGs: {chosen_string}')
         return chosen_string
     
-    def loop(self,sec,rps): 
-        driver = self.driver
-        self.sec = sec
-        self.rps = rps
-        count = 0
-
-        while count < rps:
-            print(count)
-            sleep(sec)
-            escolhidos = self.ToChoose("""self.nomes""")
-        """
-            subitComment = WebDriverWait(driver,10).until(
-            EC.element_to_be_clickable((By.XPATH,"//*[@id='mount_0_0_wl']/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[4]/section/div/form/div/div[2]/div"))
-            )
-            subitComment.click()
-            count = count +1
-        return escolhidos
-        """
-
     def writer(self,word):
         #quando for chamar a função writer, colocar como parâmetro 'word' a função ToChoose(n) para escrever a partir dos IGs
 
@@ -113,20 +85,26 @@ class InstagramBot():
         text_field = self.getField()
 
         for char in word:
-            sleep(random.uniform(0.1,1.2))
+            sleep(random.uniform(0.1,0.9))
             text_field.send_keys(char)
             #print(char)
 
-    def main(self):
+    def main(self,n,time,link):
         #Função principal onde se encontra o comando que realmente faz o código funcionar!
+        #'n' é a qtd de @s
+        #'time' é o intervalo entre envios
+        #'link' é o link do sorteio
         driver = self.driver
         self.Login()
-        self.GoToLink('https://www.instagram.com/p/BnzzZb_B1gu/')
-        self.getField()
-        self.writer(self.ToChoose(5))
+        self.GoToLink(link)
+        counter = 0
+        while counter < n:
+            self.getField()
+            self.writer(self.ToChoose(3))
+            self.SendComment()
+            sleep(time)
+            counter += 1
+        print(f'{self}: Fim de ciclo')
 
-        pass
-
-bot =  InstagramBot('rodriguesgabrielxx1999','040599gkar')
+bot =  InstagramBot()
 bot.main()
-
