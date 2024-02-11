ca11273e81f0d64820138004896cea0916df44b8
Gabriel ROdrigues <gabrielkaique_contato@hotmail.com>
2024-02-11 00:25:40 -0300

    alterações e melhorias

--- instagrambot.py
+++ instagrambot.py
@@ -25,7 +25,7 @@ class InstagramBot():
         password_field.send_keys(self.password)
         sleep(3)
         password_field.send_keys(Keys.RETURN)
-        sleep(10)
+        sleep(20)
         print(f'{self}: Login bem sucedido')
 
     def GoToLink(self,link):
@@ -49,62 +49,67 @@ class InstagramBot():
 
     def SendComment(self):
         driver = self.driver
-        
+
         #função que encontra o botão de enviar comentário e clica nele para enviar! é necessário separar ele da função getField para não haver problemas de continuidade!
 
         submitComment = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div/div[4]/section/div/form/div/textarea')
+        sleep(1)
         submitComment.send_keys(Keys.RETURN)
         print(f"{self}: comentário enviado com sucesso")
+        sleep(3)
         #submitComment.clear()
 
-    def ToChoose(self, num_to_choose):
-        #em nomes será adicionado os IG's
-        nomes = ["Miguel", "Arthur", "Gael", "Théo", "Heitor","Ravi","Davi","Bernardo","jovercino","amanda","puta que pariu","joberson","johnny","lucas"]
-        
-        #verifica se o valor num_to_choose é menor que o comprimento da lista nomes
-        num_to_choose = min(num_to_choose, len(nomes))
+    def ToChoose(self,num_to_choose):
+        # Lista de nomes de usuários do Instagram
+        nomes = ["crisiane_melissas", "eu_fafa_", "naaty._ap", "lai.ribeiroo", "eu_pedagogazi", "danioliveiras22", "admilton.gr", "fatinhaaparecida_", "sanndymarinho", "eusoumah_delgado35"]
 
         chosen = []
+        if num_to_choose <= 1:
+            escolhido = random.choice(nomes)
+            escolhido_com_arroba = "@" + escolhido
+            chosen.append(escolhido_com_arroba)
+            chosen_string = ''.join(chosen)
+            return chosen_string
+        else:
+            num_to_choose = min(num_to_choose, len(nomes))# Escolhe os nomes não adicionados na lista chosen
+            while len(chosen) < num_to_choose:
+                escolhido = random.choice(nomes)
+                # Adiciona "@" ao início do nome escolhido
+                escolhido_com_arroba = "@" + escolhido
+                if escolhido_com_arroba not in chosen:
+                    chosen.append(escolhido_com_arroba)
+            
+            # Concatena os elementos da lista chosen em uma única string separada por espaços
+            chosen_string = ' '.join(chosen)
+            return chosen_string
 
-        #escolhe os nomes não adicionados na lista chosen
-        while len(chosen) < num_to_choose:
-            escolhido = random.choice(nomes)   
-            if escolhido not in chosen:
-                chosen.append(escolhido)
-        # Concatena os elementos da lista chosen em uma única string separada por espaços
-        chosen_string = ' '.join(chosen)
-        print(f'{self}: IGs: {chosen_string}')
-        return chosen_string
-    
     def writer(self,word):
         #quando for chamar a função writer, colocar como parâmetro 'word' a função ToChoose(n) para escrever a partir dos IGs
-
         driver = self.driver
         self.word = word
-
         text_field = self.getField()
-
         for char in word:
-            sleep(random.uniform(0.1,0.9))
+            text_field.clear()
+            sleep(random.uniform(0.1,0.5))
             text_field.send_keys(char)
-            #print(char)
 
-    def main(self,n,time,link):
+    def main(self,cicle,restTime,igs,link):
         #Função principal onde se encontra o comando que realmente faz o código funcionar!
-        #'n' é a qtd de @s
+        #'n' é a quantidade de ciclos
         #'time' é o intervalo entre envios
         #'link' é o link do sorteio
         driver = self.driver
         self.Login()
         self.GoToLink(link)
         counter = 0
-        while counter < n:
+        while counter < cicle:
             self.getField()
-            self.writer(self.ToChoose(3))
+            self.writer(self.ToChoose(igs))
+            sleep(2)
             self.SendComment()
-            sleep(time)
+            sleep(restTime)
             counter += 1
         print(f'{self}: Fim de ciclo')
 
-bot =  InstagramBot()
-bot.main()
+#bot =  InstagramBot('Gabriel_AutoTester','040599gkar')
+#bot.main(5,30,1,'https://www.instagram.com/p/BLhm1GpAK9V/')
